7a9fe6cb90dbddb4f6f84370891b32f3653f2cb1
Ali Olleik <ali.oleik.2000@gmail.com>
2024-08-02 23:29:37 +0300

    reading data from excel file

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const xlsx= require("xlsx");
 const app = express();
+
 //require('dotenv').config()
 const PORT = process.env.PORT || 3030;
 //console.log(process.env.ali);
+let wb=xlsx.readFile("data.xlsx");
+let ws=wb.Sheets["Sheet1"];
+let data=xlsx.utils.sheet_to_json(ws);
+console.log(data);
+
 
 // your code
 
@@ -11,9 +18,10 @@ app.get('/', function (req, res) {
   })
 
  app.get('/students', function (req, res) {
-  var data=[{id:1,name:"ali"},{id:2,name:"mhmd"},
-    {id:3,name:"dania"},{id:4,name:"kristina"}
-  ]
+  // var data=[{id:1,name:"ali"},{id:2,name:"mhmd"},
+  //   {id:3,name:"dania"},{id:4,name:"kristina"}
+  // ]
+  
     res.send(data);
   })
 
