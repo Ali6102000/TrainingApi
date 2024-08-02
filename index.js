dbdf94d1a20ade17cd43eea3153c4bf5801adad0
Ali Olleik <ali.oleik.2000@gmail.com>
2024-08-02 16:12:00 +0300

    Adding new endpoint

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-require('dotenv').config()
+//require('dotenv').config()
 const PORT = process.env.PORT || 3030;
 //console.log(process.env.ali);
 
@@ -8,6 +8,13 @@ const PORT = process.env.PORT || 3030;
 
 app.get('/', function (req, res) {
     res.send('Hello World')
+  })
+
+ app.get('/students', function (req, res) {
+  var data=[{id:1,name:"ali"},{id:2,name:"mhmd"},
+    {id:3,name:"dania"},{id:4,name:"kristina"}
+  ]
+    res.send(data);
   })
 
 app.listen(PORT, () => {
